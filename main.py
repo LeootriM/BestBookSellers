d897259a45400811e8b424f0160cb0a2b6b102a0
Student <leotrim@gmail.com>
2025-04-19 03:48:45 -0700

    ..

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def root():
+    return{"message": "Hello Worldu"}
+
+@app.get("/greet/")
+def read_root(name: str):
+    return{"message:" f"Hello , {name}!"}
