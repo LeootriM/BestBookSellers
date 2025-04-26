34d24576e1689fdaac904ac4675ceeb9ca09b268
Student <studeng@gmail.com>
2025-04-26 03:41:20 -0700

    ..

---
+++ Type_hints.py
@@ -0,0 +1,12 @@
+
+
+def get_fullname(first_name = str , last_name = str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+print(get_fullname("john" , "pork"))
+
+def get_items(item1 = str , item2 = int , item3 = float, item4 = bool):
+    return get_items()
+
+print(get_items("Jphn" , 1 , 1.5 , False))
--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def root():
 @app.get("/greet/")
 def read_root(name: str):
     return{"message:" f"Hello , {name}!"}
+
+
+@app.get("/items/")
+def read_items():
+    return {"items": ["item1" , "item2" ," item3"] }
+
